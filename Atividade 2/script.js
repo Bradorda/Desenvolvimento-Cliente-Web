b1a319fdeea505e645af25ed0cd1ae4a8211b967
Bradorda <102385138+Bradorda@users.noreply.github.com>
2024-10-02 20:22:06 -0300

    Update script.js
    Explicando os exercícios 2 e 3;

--- Atividade 2/script.js
+++ Atividade 2/script.js
@@ -2,15 +2,27 @@
 //Exercicio 2 -----------------------------------------------------------------------------
 let elementos = ['Agua','Fogo','Ar','Terra'];
 
+/*Percorre os indices do array, que são utilizados para acessar os valores do array elementos*/
 for (let indice in elementos){
     document.write('<p>'+elementos[indice]+'</p>')
 }
-
+/*Percorre diretamente os valores do array sem precisar usar incices */
 for (let elemento of elementos){
     document.write('<p>'+elemento+'</p>')
 }
 
 //Exercicio 3 -----------------------------------------------------------------------------
+/*
+Incializa uma variavel i (indice) atrinbuindo o valor 0, 
+A variavél numeroo(contador) guarda o valor informado pelo  usuario através do prompt.
+
+Enquanto o numeroo(contador) for menor ou igual a 10{
+escreve um paragrafo, com o conteudo Linha e valor da variavél i
+incrementa o valor da variavel i (indice que será impresso)
+incrementa o valor da variavel numeroo (contador)
+repete o processo, Enquanto...
+}
+*/
 let i =0;
 let numeroo = prompt('Exercicio 3 - insira um valor');
 while(numeroo<=10){
